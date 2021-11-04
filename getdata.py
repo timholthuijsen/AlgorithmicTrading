4af4342df189275f0c42eab485680e6eedbbfef1
timholthuijsen <timholthuijsen@gmail.com>
2021-11-04 03:52:42 +0100

    Adding ability to get live data for multiple tickers at once

--- getdata.py
+++ getdata.py
@@ -5,22 +5,25 @@ from buying import *
 
 
 #live price updates and sends a signal when goal is met
-def get_live_price(ticker, goal = 1000000, 
+def get_live_price(tickers, goal = 1000000, 
                    continuous = True, refresh = 5):
     while continuous:
-        live_price = pd.DataFrame()
-        live_price[ticker] = yf.download(ticker, period='5m',interval='1m')['Adj Close']
-        price_data = live_price.iloc[-1]
-        for item in price_data:
-            price = item
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(ticker, 'at', current_time,'Price: ', price)
-        #keep it live refreshed
-        if price > goal:
-            refresh = 0.1
-            continuous = False
-        time.sleep(refresh)
+        for ticker in tickers:
+            live_price = pd.DataFrame()
+            live_price[ticker] = yf.download(ticker, period='5m',interval='1m')['Adj Close']
+            price_data = live_price.iloc[-1]
+            for item in price_data:
+                price = item
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(ticker, 'at', current_time,'Price: ', price)
+            #keep it live refreshed
+            if price > goal:
+                refresh = 0.1
+                continuous = False
+            time.sleep(refresh)
+            
+    #exit while-loop when goal is met        
     print(ticker, 'Loop exited at', current_time,
           'Price: ', price)
     signal = 'sell'
